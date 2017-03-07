c2ae4b8eb540d24bbc7eb1ba08e2125f94d5ea65
DongSoo Lee <bryanlee@zigbang.com>
2017-03-07 16:14:28 +0900

    MTKView & MTKViewDelegate  - start & stop motion of Metal drawing

--- ZBMetal/ZBMTKViewDelegate.swift
+++ ZBMetal/ZBMTKViewDelegate.swift
@@ -8,38 +8,63 @@
 
 import MetalKit
 
-public class ZBMTKViewDelegate: NSObject, MTKViewDelegate {
+public class ZBMTKViewDelegate: NSObject, MTKViewDelegate, UIGestureRecognizerDelegate {
 
     var device: MTLDevice!
     var queue: MTLCommandQueue!
     var cps: MTLComputePipelineState!
+
+    var timer: Float = 0
+    var timerBuffer: MTLBuffer!
+    var pos: CGPoint!
+    var mouseBuffer: MTLBuffer!
+
+    var tapGesture: UITapGestureRecognizer!
 
     public override init() {
         super.init()
 
         self.device = MTLCreateSystemDefaultDevice()
         self.registerShaders()
+
+        self.tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
     }
 
     func registerShaders() {
         self.queue = self.device.makeCommandQueue()
 
         guard let library = self.device.newDefaultLibrary() else { return }
-        let kernel = library.makeFunction(name: "compute")
+        let kernel = library.makeFunction(name: "computeMin")
 
         do {
             self.cps = try self.device?.makeComputePipelineState(function: kernel!)
         } catch let error {
             print("\(error)")
         }
+        self.timerBuffer = self.device?.makeBuffer(length: MemoryLayout<Float>.size, options: [])
+        self.mouseBuffer = self.device?.makeBuffer(length: MemoryLayout<CGPoint>.size, options: [])
+    }
+
+    func update() {
+        self.timer += 0.01
+        var bufferPointer = self.timerBuffer.contents()
+        memcpy(bufferPointer, &self.timer, MemoryLayout<Float>.size)
+        bufferPointer = self.mouseBuffer.contents()
+        memcpy(bufferPointer, &self.pos, MemoryLayout<CGPoint>.size)
     }
 
     public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         //
     }
 
+    var drawnView: MTKView!
+
     public func draw(in view: MTKView) {
         view.framebufferOnly = false
+
+        view.addGestureRecognizer(self.tapGesture)
+        self.drawnView = view
+
         guard let drawable = view.currentDrawable else { return }
 
         let commandBuffer = self.queue.makeCommandBuffer()
@@ -47,6 +72,9 @@ public class ZBMTKViewDelegate: NSObject, MTKViewDelegate {
 
         encoder.setComputePipelineState(self.cps)
         encoder.setTexture(drawable.texture, at: 0)
+        encoder.setBuffer(self.timerBuffer, offset: 0, at: 1)
+        encoder.setBuffer(self.mouseBuffer, offset: 0, at: 2)
+        self.update()
 
         let threadGroupCount = MTLSizeMake(8, 8, 1)
         let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
@@ -55,5 +83,14 @@ public class ZBMTKViewDelegate: NSObject, MTKViewDelegate {
         encoder.endEncoding()
         commandBuffer.present(drawable)
         commandBuffer.commit()
+    }
+
+    func handleTap(gesture: UITapGestureRecognizer) {
+        if gesture.state == .ended {
+            self.pos = gesture.location(in: self.drawnView)
+            let scale = self.drawnView.layer.contentsScale
+            pos.x *= scale
+            pos.y *= scale
+        }
     }
 }
